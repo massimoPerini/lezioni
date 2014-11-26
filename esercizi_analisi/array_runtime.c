d03c62ab114fc399789f625c40b5d1e614cf528a
Massimo Perini <perinimassimo1995@gmail.com>
2014-11-26 14:12:08 +0100

    Risolto

--- esercizi_analisi/array_runtime.c
+++ esercizi_analisi/array_runtime.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main()
+int main()      //Scrive i primi n-1 numeri interi
 {
     int *a;
     int n,i;
-
+    
     printf ("quanti numeri interi vuoi che memorizzi?");
     scanf("%i",&n);
-
+    
     a=malloc(n*sizeof(int));
-
+    
     for (i=0;i<n;i++)
         a[i]=i;
-
+ 
     for (i=0;i<n;i++)
         printf ("%i\n",a[i]);
-
-    free(p);
-
-}
+    
+    //free(p);        //p non è definita!!! Era a
+    free(a)
