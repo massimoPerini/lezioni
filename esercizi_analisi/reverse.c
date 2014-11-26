d70659e9261b0d8b5d82c2435451211fb260d218
Massimo Perini <perinimassimo1995@gmail.com>
2014-11-26 22:15:21 +0100

    Risolto

--- esercizi_analisi/reverse.c
+++ esercizi_analisi/reverse.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 
-char* f(char* s, char c)
+char* f(char* s, char c)    //trova l'indice di c all'interno di una stringa (o arriva alla fine della stringa)
 {
-	for (; *s!=c && *s!='\0'; s++) ;
-	
-	if (*s!='\0') s++;
-		
-	return s;
+    for (; *s!=c && *s!='\0'; s++) ;
+    
+    if (*s!='\0') s++;
+    
+    return s;
 }
 
-char* p(char* s, char c)
+void p(char* s, char c)     //stampa tutti i caratteri fino al carattere c o fino alla fine della stringa
 {
-	for (; *s!=c && *s!='\0'; s++)
-		printf ("%c",*s);
+    for (; *s!=c && *s!='\0'; s++)
+        printf ("%c",*s);
 }
 
-void g(char* s, char c)
+void g(char* s, char c)     //'spezza' la stringa sul carattere e inverte i pezzi
 {
-	if (*s=='\0') return;
-	
-	g(f(s,c),c);
-	p(s,c);
+    if (*s=='\0') return;
+    
+    g(f(s,c),c);
+    
+    /*
+     vengono generati un certo numero di funzioni g, ognuna con s che punta allo spazio o a 0. Quando arriva a 0
+     le chiamate ricorsive terminano, e quindi con la prossima istruzione viene stampata la stringa dal carattere c alla
+     fine e in seguito dal pezzo ancora minore a c ecc...
+     */
+    
+    p(s,c);
 }
 
 int main()
 {
-	g("sei uno zero",' ');
-	printf ("\n");
+    g("sei uno zero",' ');
+    printf ("\n");
 }
