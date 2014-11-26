003bea7dcadabe61e07be4f26ccf64eedd147860
Massimo Perini <perinimassimo1995@gmail.com>
2014-11-26 21:00:11 +0100

    Risolto

--- esercizi_analisi/palindromo.c
+++ esercizi_analisi/palindromo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int r(int n)
+int r(int n)    //somma tutte le cifre e moltiplica per 10, eccetto la più rilevante, che viene aggiunta alla fine
 {
     if (n<10)
         return n;
@@ -11,5 +11,5 @@ int main()
 {
     int i;
     for (i=0;i<101;i++)
-        if (i==r(i)) printf("%i\n",i);
+        if (i==r(i)) printf("%i\n",i);          //stampa solo i numeri che una volta invertite le cifre sono uguali al numero di partenza
 }
