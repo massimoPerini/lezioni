213203e4a026e7708294c161903bd9ab922b0875
Massimo Perini <perinimassimo1995@gmail.com>
2014-11-26 14:09:17 +0100

    Risolto

--- esercizi_analisi/anagram.c
+++ esercizi_analisi/anagram.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
-
-
-int f(int n)
+int f(int n)            //Somma dei divisori di n
 {
-	int s=0;
-	int i;
-	for (i=1;i<n;i++)
-		if (!(n%i))
-			s+=i;
-			
-	return s;
+    int s=0;
+    int i;
+    for (i=1;i<n;i++)
+        if (!(n%i))
+            s+=i;
+    
+    return s;
 }
-
-int g(int n)
+int g(int n)            //Controlla se il numero è uguale alla somma dei suoi divisori
 {
-	return (f(n)==n);
+    return (f(n)==n);
 }
-
-
 int main(int argc,char** argv)
 {
-	int i;
-	int a[6]={1,13,15,6,28,1000};
-	
-	for (i=0;i<6;i++)
-		if (g(a[i])) printf ("%i\n",a[i]);
+    int i;
+    int a[6]={1,13,15,6,28,1000};
+    
+    for (i=0;i<6;i++)
+        if (g(a[i])) printf ("%i\n",a[i]);
 }
