bea74a8150c2d3847901e19cd77eea3744d8579e
Massimo Perini <perinimassimo1995@gmail.com>
2014-11-26 18:31:58 +0100

    Risolto

--- esercizi_analisi/incbis.c
+++ esercizi_analisi/incbis.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void i(char* s, char b, int c)
+void i(char* s, char b, int c)  //sostituisce a s il valore di b per tutti i caratteri fino a quando non trova un carattere la cui differenza con b è > di c-1 (o la stringa finisce)
 {
     if (*s=='\0') return;
     if ((*s-b)<c-1) (*s)++;
@@ -16,5 +16,5 @@ int main(int argc, char** argv)
     char *s=strdup("xxx"), c=*s;
     int j;
     for (j=0;j<12;j++,i(s,c,3))
-        printf ("%s\n",s);
+        printf ("%s\n",s);      //xxx - yxx - zxx -  -xyx
 }
