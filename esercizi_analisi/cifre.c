74e8041afa3dabafee8fd8a6581da3773ff0acc9
Massimo Perini <perinimassimo1995@gmail.com>
2014-11-26 14:13:23 +0100

    Risolto

--- esercizi_analisi/cifre.c
+++ esercizi_analisi/cifre.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
-int f(int n, int c)
+int f(int n, int c)     //Quante volte è presente c nel numero
 {
     if (n==0) return 0;
     if (n%10 == c) return (1 + f(n/10, c));
     else return f(n/10, c);
 }
-
-
 int main()
 {
     printf("%i\n", f(12345,1));
