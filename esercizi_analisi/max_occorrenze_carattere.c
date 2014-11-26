c8b8756a05ead68927441fdfce1cf8b6c525710a
Massimo Perini <perinimassimo1995@gmail.com>
2014-11-26 20:43:38 +0100

    Risolto

--- esercizi_analisi/max_occorrenze_carattere.c
+++ esercizi_analisi/max_occorrenze_carattere.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
-int f(char a[], char b)
+int g(char a, char s[], int i)      //Conta quante volte il carattere a è presente in s a partire da un certo indice
 {
-    int i, n=0;
-    for (i=0;a[i]!='\0';i++)
-        if (a[i]==b) n++;
+    int n=0;
+    for (;s[i]!='\0';i++)
+        if (a==s[i]) n++;
     return n;
 }
 
-char g(char a[])
+int f (char s[])    //Conta quanti caratteri diversi ci sono nella stringa
 {
-    char c; int i,m=0;
-    for (i=0;a[i]!='\0';i++)
-        if (f(a,a[i])>m)
-        { c=a[i]; m=f(a,a[i]);}
-    return c;
+    int n=0,i;
+    for (i=0;s[i]!=0;i++)
+        if (g(s[i],s,i)==1){
+            n++;
+        }
+    return n;
 }
 
 int main()
 {
-    printf ("%c\n",g("quare id faciam"));
-    printf ("%c\n",g("fortasse requiris"));
-    printf ("%c\n",g("nescio, sed fieri sentior, et exrucior"));
+    printf ("%i, %i, %i\n",f("quare id faciam"), f("fortasse requiris"),
+            f("nescio, sed fieri sentio"));
+    
 }
-
