7128ef42d9dbdaa332ec875e2b1a26671ec91cec
Massimo Perini <perinimassimo1995@gmail.com>
2014-11-26 18:09:34 +0100

    Risolto

--- esercizi_analisi/inc.c
+++ esercizi_analisi/inc.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+//La funzione i è sbagliata perchè non ritorna nulla mentre è dovrebbe ritornare un puntatore a char
+
 char* i(char* s, char b)
 {
     if (*s=='\0') return;
