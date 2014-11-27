248511d3c05d42f4fe122cdaf4900ff9c2e2ef40
Massimo Perini <perinimassimo1995@gmail.com>
2014-11-27 11:05:46 +0100

    Risolto

--- esercizi_analisi/tartaglia.c
+++ esercizi_analisi/tartaglia.c
@@ -1,46 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* t(int n,int* a)
+int* t(int n,int* a)    //Array di n+1 che inizia e finisce con uno e il resto è la somma della posizione di a e a-1
 {
-	int i;
-	int *r=(int*)malloc(sizeof(int)*(n+1));
-	r[0]=1;
-	for (i=1;i<n;i++)
-		r[i]=a[i-1]+a[i];
-	r[n]=1;
-	return r;
+    int i;
+    int *r=(int*)malloc(sizeof(int)*(n+1));
+    r[0]=1;
+    for (i=1;i<n;i++)
+        r[i]=a[i-1]+a[i];   //Somma il numero attuale e il precedente
+    r[n]=1;
+    return r;
 }
 
-void s(int n, int* a)
+void s(int n, int* a)   //Stampa il contenuto dell'array
 {
-	int i;
-	for (i=0;i<=n;i++)
-		printf ("%d\t",a[i]);
-	
-	printf ("\n");
+    int i;
+    for (i=0;i<=n;i++)
+        printf ("%d\t",a[i]);
+    
+    printf ("\n");
 }
 
-void tr(int n)
+void tr(int n)  //Scrive il triangolo di tartaglia
 {
-	int *p=(int*)malloc(sizeof(int));
-	int* tmp;
-	int i;
-	
-	p[0]=1;
-	
-	s(0,p);
-	
-	for (i=1;i<=n;i++)
-	{
-		p=t(i,p);
-		s(i,p);
-	}
+    int *p=(int*)malloc(sizeof(int));
+    int i;
+    
+    p[0]=1;
+    
+    s(0,p);
+    
+    for (i=1;i<=n;i++)
+    {
+        p=t(i,p);   //[1,1] [1,2,1] [1,3,3,1] [1,4,6,4,1] ecc...
+        s(i,p); //Mostra l'array
+    }
 }
 
 int main(int argc,char** argv)
 {
-	tr(5);
+    tr(5);
 }
 		
 		
